98daa76317cd3b44954c68b59b71eef74bb84350
BiPhan4 <biphan374@gmail.com>
2023-12-20 09:26:07 -0500

    split tests into different files

--- e2e/interchaintest/contract_handshake_test.go
+++ e2e/interchaintest/contract_handshake_test.go
@@ -68,7 +68,7 @@ func (s *ContractTestSuite) TestIcaContractChannelHandshake() {
 
 	// This starts the chains, relayer, creates the user accounts, creates the ibc clients and connections,
 	// sets up the contract and does the channel handshake for the contract test suite.
-	s.SetupContractTestSuite(ctx, icatypes.EncodingProtobuf)
+	s.SetupContractTestSuite(ctx, icatypes.EncodingProtobuf) // NOTE: canined's ibc-go is outdated and does not support proto3json
 	wasmd, canined := s.ChainA, s.ChainB
 
 	s.Run("TestChannelHandshakeSuccess", func() {
---
+++ e2e/interchaintest/send_with_protobuf_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	icatypes "github.com/cosmos/ibc-go/v7/modules/apps/27-interchain-accounts/types"
+	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types/v1beta1"
+
+)
+
+func (s *ContractTestSuite) IcaContractExecutionTestWithProtobuf() {
+	ctx := context.Background()
+
+
+
+
+
+)
+
+}
