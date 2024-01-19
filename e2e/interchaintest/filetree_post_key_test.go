8522e7cf384d832d76633edbedf2a5f95f1ed7ce
BiPhan4 <biphan374@gmail.com>
2024-01-19 12:13:21 -0500

    test filetree post key message from wasmd to canined

---
+++ e2e/interchaintest/filetree_post_key_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/cosmos/gogoproto/proto"
+	icatypes "github.com/cosmos/ibc-go/v7/modules/apps/27-interchain-accounts/types"
+
+	logger "github.com/JackalLabs/storage-outpost/e2e/interchaintest/logger"
+
+	filetreetypes "github.com/jackalLabs/canine-chain/v3/x/filetree/types"
+)
+
+// WARNING: strangelove's test package builds chains running ibc-go/v7
+// Hopefully this won't cause issues because the canined image we use is running ibc-go/v4
+// and packets should be consumed by the ica host no matter what version of ibc-go the controller chain is running
+
+func (s *ContractTestSuite) TestIcaContractExecutionTestWithFiletree() {
+	ctx := context.Background()
+
+	logger.InitLogger()
+
+	encoding := icatypes.EncodingProtobuf
+	// This starts the chains, relayer, creates the user accounts, creates the ibc clients and connections,
+	// sets up the contract and does the channel handshake for the contract test suite.
+	s.SetupContractTestSuite(ctx, encoding)
+	_, canined := s.ChainA, s.ChainB
+	wasmdUser := s.UserA
+
+	logger.LogInfo(canined.FullNodes)
+
+	// Fund the ICA address:
+	s.FundAddressChainB(ctx, s.IcaAddress)
+
+	// Give canined some time to complete the handshake
+	time.Sleep(time.Duration(30) * time.Second)
+
+	s.Run(fmt.Sprintf("TestSendCustomIcaMesssagesSuccess-%s", encoding), func() {
+		filetreeMsg := filetreetypes.MsgPostkey{
+			Creator: wasmdUser.FormattedAddress(), // This will soon be the contract address
+			Key:     "Hey it's Bi from the outpost on another chain. We reached filetree!!! <3",
+		}
+
+		// Execute the contract:
+		err := s.Contract.ExecCustomIcaMessages(ctx, wasmdUser.KeyName(), []proto.Message{&filetreeMsg}, encoding, nil, nil)
+		s.Require().NoError(err)
+
+		// We haven't implemented call backs so at this point we could just start a shell session in the container to
+		// view the filetree entry
+
+	},
+	)
+
+	time.Sleep(time.Duration(1) * time.Hour)
+
+}
--- e2e/interchaintest/go.mod
+++ e2e/interchaintest/go.mod
@@ -1,6 +1,8 @@
 module github.com/JackalLabs/storage-outpost/e2e/interchaintest
 
-go 1.20
+go 1.21
+
+toolchain go1.21.3
 
 require (
 	cosmossdk.io/math v1.1.2
@@ -8,6 +10,7 @@ require (
 	github.com/cosmos/gogoproto v1.4.10
 	github.com/cosmos/ibc-go/v7 v7.3.0
 	github.com/docker/docker v24.0.7+incompatible
+	github.com/jackalLabs/canine-chain/v3 v3.0.0
 	github.com/strangelove-ventures/interchaintest/v7 v7.0.0
 	github.com/stretchr/testify v1.8.4
 	go.uber.org/zap v1.26.0
@@ -83,7 +86,8 @@ require (
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/dvsekhvalnov/jose2go v1.5.0 // indirect
-	github.com/ethereum/go-ethereum v1.11.2 // indirect
+	github.com/ecies/go/v2 v2.0.6 // indirect
+	github.com/ethereum/go-ethereum v1.11.5 // indirect
 	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/getsentry/sentry-go v0.23.0 // indirect
@@ -192,8 +196,8 @@ require (
 	github.com/tyler-smith/go-bip32 v1.0.0 // indirect
 	github.com/tyler-smith/go-bip39 v1.1.0 // indirect
 	github.com/ulikunitz/xz v0.5.11 // indirect
-	github.com/zondax/hid v0.9.1 // indirect
-	github.com/zondax/ledger-go v0.14.1 // indirect
+	github.com/zondax/hid v0.9.2 // indirect
+	github.com/zondax/ledger-go v0.14.3 // indirect
 	go.etcd.io/bbolt v1.3.7 // indirect
 	go.opencensus.io v0.24.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
