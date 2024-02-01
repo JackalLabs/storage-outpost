28f63d7ee4fc07071d6b29198585f29869d0c987
BiPhan4 <biphan374@gmail.com>
2024-02-01 16:06:42 -0500

    add basic jackal encoding

---
+++ e2e/interchaintest/types/basic_jackal_encoding.go
@@ -0,0 +1,32 @@
+package types
+
+import (
+	moduletestutil "github.com/cosmos/cosmos-sdk/types/module/testutil"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/cosmos/cosmos-sdk/x/bank"
+	"github.com/cosmos/cosmos-sdk/x/capability"
+	"github.com/cosmos/cosmos-sdk/x/consensus"
+	"github.com/cosmos/cosmos-sdk/x/mint"
+	"github.com/cosmos/cosmos-sdk/x/params"
+	"github.com/cosmos/cosmos-sdk/x/slashing"
+	"github.com/cosmos/cosmos-sdk/x/staking"
+
+	"github.com/jackalLabs/canine-chain/v3/x/filetree"
+)
+
+func JackaklEncoding() *moduletestutil.TestEncodingConfig {
+
+	encCfg := moduletestutil.MakeTestEncodingConfig(
+		auth.AppModuleBasic{},
+		bank.AppModuleBasic{},
+		capability.AppModuleBasic{},
+		staking.AppModuleBasic{},
+		mint.AppModuleBasic{},
+		params.AppModuleBasic{},
+		slashing.AppModuleBasic{},
+		consensus.AppModuleBasic{},
+		filetree.AppModuleBasic{},
+	)
+
+	return &encCfg
+}
