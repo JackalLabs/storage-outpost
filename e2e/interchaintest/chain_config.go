4593e411cf87f308ac7060a7b18999df203b0e61
BiPhan4 <biphan374@gmail.com>
2024-04-08 14:34:00 -0400

    update storage genesis params for test suite

--- e2e/interchaintest/chain_config.go
+++ e2e/interchaintest/chain_config.go
@@ -33,6 +33,31 @@ var genesisAllowICH = map[string]interface{}{
 			"port": "icahost",
 		},
 	},
+	"storage": map[string]interface{}{
+
+		"active_providers_list": []interface{}{},
+		"attest_forms":          []interface{}{},
+		"collateral_list":       []interface{}{},
+		"file_list":             []interface{}{},
+
+		"params": map[string]interface{}{
+			"attestFormSize":             "5",
+			"attestMinToPass":            "3",
+			"check_window":               "100",
+			"chunk_size":                 "1024",
+			"collateralPrice":            "10000000000",
+			"deposit_account":            "jkl12g4qwenvpzqeakavx5adqkw203s629tf6k8vdg", // Let's deposit all storage payments to the Danny user
+			"max_contract_age_in_blocks": "100",
+			"misses_to_burn":             "3",
+			"price_feed":                 "jklprice",
+			"price_per_tb_per_month":     "8",
+			"proof_window":               "50",
+		},
+
+		"payment_info_list": []interface{}{},
+		"providers_list":    []interface{}{},
+		"report_forms":      []interface{}{},
+	},
 }
 
 var chainSpecs = []*interchaintest.ChainSpec{
