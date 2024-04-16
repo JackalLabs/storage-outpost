ea919a140f2e0bc66909380261ae5d047f4af665
BiPhan4 <biphan374@gmail.com>
2024-04-16 14:41:10 -0400

    add filetree MsgPostFile proto definition

--- src/types/filetree.rs
+++ src/types/filetree.rs
@@ -21,3 +21,65 @@ pub struct MsgPostKey {
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct MsgPostKeyResponse {}
+
+/// TODO: update struct documentation comments for v4 chain upgrade 
+/// The below are just placeholder comments.
+/// documentation for the filetree module can be found here:
+/// https://github.com/JackalLabs/canine-chain/tree/master/x/filetree
+
+/// Post a Files struct to chain 
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct MsgPostFile {
+    /// The creator and broadcaster of this message. Pass in alice's Bech32 address
+    #[prost(string, tag = "1")]
+    pub creator: ::prost::alloc::string::String,
+
+    /// Hex[ hash( alice's Bech32 address )]
+    #[prost(string, tag = "2")]
+    pub account: ::prost::alloc::string::String,
+
+    /// MerklePath("s")
+    #[prost(string, tag = "3")]
+    pub hash_parent: ::prost::alloc::string::String,
+
+    /// Hex[ hash("home") ]
+    #[prost(string, tag = "4")]
+    pub hash_child: ::prost::alloc::string::String,
+
+    /// FID
+    #[prost(string, tag = "5")]
+    pub contents: ::prost::alloc::string::String,
+
+    /// string(json encoded map) with: 
+    /// let c = concatenate( "v", trackingNumber, Bech32 address )
+    /// map_key: hex[ hash("c") ]
+    /// map_value: ECIES.encrypt( aesIV + aesKey )
+    /// Note that map_key and map_value must be strings or else unmarshalling in the keeper will fail.
+
+    #[prost(string, tag = "6")]
+    pub viewers: ::prost::alloc::string::String,
+
+    /// same as above but with c = concatenate( "e", trackingNumber, Bech32 address )
+    #[prost(string, tag = "7")]
+    pub editors: ::prost::alloc::string::String,
+
+    /// UUID. This trackingNumber is one and the same as what is used in editors AND viewers map
+    #[prost(string, tag = "8")]
+    pub tracking_number: ::prost::alloc::string::String,
+}
+
+/// The response object 
+/// let fullMerklePath = MerklePath("s/home")
+/// 
+/// {
+///     "path": "fullMerklePath"
+/// }
+
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct MsgPostFileResponse {
+    /// fullMerklePath
+    #[prost(string, tag = "1")]
+    pub path: ::prost::alloc::string::String,
+}
