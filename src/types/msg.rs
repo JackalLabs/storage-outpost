f6a3be7103ebb7fdf5cab1c68758e7e6491e6adc
BiPhan4 <biphan374@gmail.com>
2024-10-11 12:36:54 -0400

    remove redundant query

--- src/types/msg.rs
+++ src/types/msg.rs
@@ -125,16 +125,12 @@ pub enum QueryMsg {
     /// GetChannel returns the IBC channel info.
     #[returns(crate::types::state::ChannelState)]
     GetChannel {},
-    /// GetChannelStatus uses the CosmWasm API to query the current channel status
-    #[returns(crate::types::state::ChannelState)] // Not sure what it returns at the moment
-    GetChannelStatus {},
     /// GetContractState returns the contact's state.
     #[returns(crate::types::state::ContractState)]
     GetContractState {},
     /// GetCallbackCounter returns the callback counter.
     #[returns(crate::types::state::CallbackCounter)]
     GetCallbackCounter {},
-    
 }
 
 /// The message to migrate this contract.
